2e2817d2abe32cf7fb06587c4632e7b1b006e148
Hotram89 <marion.dournel@gmail.com>
2022-04-06 14:44:19 +0200

    fixed : change Class RecipeDto

--- scripts/RecipeDto.js
+++ scripts/RecipeDto.js
@@ -5,23 +5,22 @@ class RecipeDto
     constructor(recipe) {
 
         this.id = recipe.id
+        this.time = recipe.time
         this.name = recipe.name
-        console.log(recipe.name);
         this.servings = recipe.servings
         this.ingredients = new Set()
-        // recipe.ingredients.forEach((ingredient) => {
-        //     this.ingredients.add(ingredient)
-        // })
-
+         recipe.ingredients.forEach((ingredient) => {
+             this.ingredients.add(ingredient)
+         })
+        this.description = recipe.description
+        this.appliance = recipe.appliance
+        this.ustensils = recipe.ustensils
+        this.blob = new Set()
+        recipe.ustensils.forEach((ustensil) => {
+            this.blob.add(ustensil)
+        })
     }
-    findAll() {
-        console.log('coucou');
-    }
-    collection() {
-        const collection = new RecipeProvider
-        .getAllrecipe();
-    }
-    
+   
     getIngredient(ingredient) {
         return this.ingredients.include(ingredient)
 
--- scripts/RecipeProvider.js
+++ scripts/RecipeProvider.js
@@ -1,16 +1,22 @@
 import { recipes } from '../data/recipes.js'
+import RecipeDto from './RecipeDto.js';
 
 // class pour aller chercher les recettes
 export default
 class RecipeProvider 
 {
+    constructor() {
+        this.data = new Set()
+        recipes.forEach((recipe) => {
+            this.data.add(new RecipeDto(recipe))
+        })  
+    }
+
     // retourne un array avec toutes les recettes
     findAll() {
-        console.log(recipes);
-        return recipes 
-}
-
-   
+       
+        return this.data
+} 
 
 }
 export { RecipeProvider}
