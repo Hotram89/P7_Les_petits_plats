e553c69ab4311a539d7140b7932933f9f0ea6a91
Hotram89 <marion.dournel@gmail.com>
2022-05-16 15:56:40 +0200

    fixed = CSS for tag container and ul

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,14 @@
 function formatString(str) {
-    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim();
+	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim();
 }
 
 export function escapeArray(data) {
-    let newData = new Set();
+	let newData = new Set();
 
-    data = Array.from(data).sort();
-    data.forEach((el) => {
-        newData.add(formatString(el));
-    });
+	data = Array.from(data).sort();
+	data.forEach((el) => {
+		newData.add(formatString(el));
+	});
 
-    return newData;
+	return newData;
 }
